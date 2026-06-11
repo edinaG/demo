fbfe3c7528cae1584211b5457a8882a313ec2c08
Edina Andreicut <edina.gorog@nexource.ro>
2026-06-11 09:33:58 +0300

    initial commit

---
+++ src/main/java/ro/nxr/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package ro.nxr.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class DemoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+		return app.sources(DemoApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
